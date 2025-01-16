6e0853016e38e8dbd739208514d6b9c1aed52e40
Olivier Hermans <olivier.hermans@devoteam.com>
2025-01-16 12:57:06 +0100

    Refactor logic, add unit tests and fix expected output for Conjured items

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -8,55 +8,48 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+        for (Item item : items) {
+            item.sellIn = "Sulfuras, Hand of Ragnaros".equals(item.name) ? item.sellIn : item.sellIn - 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+            final boolean isExpired = item.sellIn < 0;
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
+            // We can also define a normal quality adjustment of -1 and double it in case of expiry:
+            //
+            // - In case of Aged Brie, we then multiply with -1 to invert the quality adjustment
+            // - In case of Conjured, we then multiply with 2 to double the quality adjustment
+            //
+            // This keeps Aged Brie and Conjured adjustments "pegged" (i.e. relative) to the normal value adjustment if
+            // that is what is required in all circumstances
+            //
+            // e.g.
+            // final int normalAdjustment = isExpired ? -2 : -1;
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+            switch (item.name) {
+//                case "Aged Brie": adjustQuality(item, isExpired ? 2 : 1); break; // or: adjustQuality(item, -1 * normalAdjustment);
+                case "Aged Brie": adjustQuality(item, isExpired ? 2 : 1); break; // or: adjustQuality(item, -1 * normalAdjustment);
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    adjustQuality(item, 1);
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                    if (item.sellIn < 10) {
+                        adjustQuality(item, 1);
                     }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+
+                    if (item.sellIn < 5) {
+                        adjustQuality(item, 1);
                     }
-                }
+
+                    if (isExpired) {
+                        item.quality = 0;
+                    }
+                    break;
+                case "Sulfuras, Hand of Ragnaros": break;
+                case "Conjured Mana Cake": adjustQuality(item, isExpired ? -4 : -2); break; // or: adjustQuality(item, 2 * normalAdjustment);
+                default: adjustQuality(item, isExpired ? -2 : -1); break; // or: adjustQuality(item, normalAdjustment);
             }
         }
+    }
+
+    static void adjustQuality(Item item, int adjustBy) {
+        item.quality = Math.clamp(item.quality + adjustBy, 0, 50);
     }
 }
--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,163 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
+    void updateQuality_agedBrieNotExpired_qualityIncreasedByOne() {
+        final GildedRose app =
+            new GildedRose(
+                buildItems(
+                    new Item("Aged Brie", 2, 2)));
+
         app.updateQuality();
-        assertEquals("foo", app.items[0].name);
+
+        assertEquals(1, app.items[0].sellIn);
+        assertEquals(3, app.items[0].quality);
     }
 
+    @Test
+    void updateQuality_agedBrieExpired_qualityIncreasedByTwo() {
+        final GildedRose app =
+            new GildedRose(
+                buildItems(
+                    new Item("Aged Brie", -1, 2)));
+
+        app.updateQuality();
+
+        assertEquals(-2, app.items[0].sellIn);
+        assertEquals(4, app.items[0].quality);
+    }
+
+    @Test
+    void  updateQuality_backstagePassesMoreThan10DaysLeft_qualityIncreasedByOne() {
+        final GildedRose app =
+            new GildedRose(
+                buildItems(
+                    new Item("Backstage passes to a TAFKAL80ETC concert", 15, 2)));
+
+        app.updateQuality();
+
+        assertEquals(14, app.items[0].sellIn);
+        assertEquals(3, app.items[0].quality);
+    }
+
+    @Test
+    void  updateQuality_backstagePassesMoreThan5AndLessThanIncluding10DaysLeft_qualityIncreasedByTwo() {
+        final GildedRose app =
+            new GildedRose(
+                buildItems(
+                    new Item("Backstage passes to a TAFKAL80ETC concert", 9, 2)));
+
+        app.updateQuality();
+
+        assertEquals(8, app.items[0].sellIn);
+        assertEquals(4, app.items[0].quality);
+    }
+
+    @Test
+    void  updateQuality_backstagePassesLessThanIncluding5DaysLeft_qualityIncreasedByThree() {
+        final GildedRose app =
+            new GildedRose(
+                buildItems(
+                    new Item("Backstage passes to a TAFKAL80ETC concert", 4, 2)));
+
+        app.updateQuality();
+
+        assertEquals(3, app.items[0].sellIn);
+        assertEquals(5, app.items[0].quality);
+    }
+
+    @Test
+    void  updateQuality_backstagePassesExpired_quality0() {
+        final GildedRose app =
+            new GildedRose(
+                buildItems(
+                    new Item("Backstage passes to a TAFKAL80ETC concert", -1, 2)));
+
+        app.updateQuality();
+
+        assertEquals(-2, app.items[0].sellIn);
+        assertEquals(0, app.items[0].quality);
+    }
+
+    @Test
+    void updateQuality_sulfuras_shouldNotChange() {
+        final GildedRose app =
+            new GildedRose(
+                buildItems(
+                    new Item("Sulfuras, Hand of Ragnaros", 10, 80)));
+
+        app.updateQuality();
+
+        assertEquals(10, app.items[0].sellIn);
+        assertEquals(80, app.items[0].quality);
+    }
+
+    @Test
+    void updateQuality_conjuredNotExpired_qualityDecreasedBy2() {
+        final GildedRose app =
+            new GildedRose(
+                buildItems(
+                    new Item("Conjured Mana Cake", 10, 10)));
+
+        app.updateQuality();
+
+        assertEquals(9, app.items[0].sellIn);
+        assertEquals(8, app.items[0].quality);
+    }
+
+    @Test
+    void updateQuality_conjuredExpired_qualityDecreasedBy4() {
+        final GildedRose app =
+            new GildedRose(
+                buildItems(
+                    new Item("Conjured Mana Cake", 0, 10)));
+
+        app.updateQuality();
+
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(6, app.items[0].quality);
+    }
+
+    @Test
+    void updateQuality_otherNotExpired_qualityDecreasedBy1() {
+        final GildedRose app =
+            new GildedRose(
+                buildItems(
+                    new Item("Some other item", 10, 10)));
+
+        app.updateQuality();
+
+        assertEquals(9, app.items[0].sellIn);
+        assertEquals(9, app.items[0].quality);
+    }
+
+    @Test
+    void updateQuality_otherExpired_qualityDecreasedBy2() {
+        final GildedRose app =
+            new GildedRose(
+                buildItems(
+                    new Item("Yet another item", 0, 10)));
+
+        app.updateQuality();
+
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(8, app.items[0].quality);
+    }
+
+    @Test
+    void adjustQuality_upperBoundRespected() {
+        final Item item = new Item("test", 2, 49);
+        GildedRose.adjustQuality(item, 2);
+        assertEquals(50, item.quality);
+    }
+
+    @Test
+    void adjustQuality_lowerBoundRespected() {
+        final Item item = new Item("test", 2, 1);
+        GildedRose.adjustQuality(item, -2);
+        assertEquals(0, item.quality);
+    }
+
+    private Item[] buildItems(Item... items) {
+        return items;
+    }
 }
